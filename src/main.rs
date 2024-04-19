3a4f7fdefb7ebf72512da4458fd5b28864f857ed
Marko <marko.uksanovic1@gmail.com>
2024-04-19 16:04:05 +1000

    cicd: added basic tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+fn add_two_numbers(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = add_two_numbers(2, 3);
+    println!("The result of adding 2 and 3 is: {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::add_two_numbers;
+
+    #[test]
+    fn test_add_two_numbers() {
+        assert_eq!(add_two_numbers(2, 3), 5);
+    }
 }
